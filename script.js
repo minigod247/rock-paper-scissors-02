bb3af62de319730f8594739636152831e34d96f4
minigod247 <minigod247@gmail.com>
2026-04-01 05:52:43 -0700

    Add computer choice logi

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random());
+
+  if (randomNumber < 0.33) {
+    return "rock";
+  } else if (randomNumber < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
